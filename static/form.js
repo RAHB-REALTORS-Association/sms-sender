f96ade42259893dc5100748053c8ec90ed4e0805
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2023-09-24 00:38:43 -0400

    Remove send validation

--- static/form.js
+++ static/form.js
@@ -1,25 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const sidInput = document.getElementById('sid');
-    const tokenInput = document.getElementById('token');
-    const csvUrlInput = document.getElementById('csv_url');
-    const fileInput = document.getElementById('file-upload');
-    const sendBtn = document.getElementById('sendBtn');
-    const checkmarkSpan = document.getElementById('checkmark');
-    const sendIcon = document.getElementById("sendIcon");
-    const spinner = document.getElementById("spinner");
-
-    function toggleSendButton() {
-        const sidHasValueOrPlaceholder = sidInput.value || sidInput.placeholder;
-        const tokenHasValueOrPlaceholder = tokenInput.value || tokenInput.placeholder;
-        
-        sendBtn.disabled = !(sidHasValueOrPlaceholder && tokenHasValueOrPlaceholder && (fileInput.files.length || csvUrlInput.value));
-    }
-
-    sidInput.addEventListener('input', toggleSendButton);
-    tokenInput.addEventListener('input', toggleSendButton);
-    csvUrlInput.addEventListener('input', toggleSendButton);
-    fileInput.addEventListener('change', toggleSendButton);
-    
     fileInput.addEventListener('change', function() {
         if (fileInput.files.length > 0) {
             checkmarkSpan.classList.remove('d-none'); // Show checkmark if a file is uploaded.
