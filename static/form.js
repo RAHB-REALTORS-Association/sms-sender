4a8dbd3c0eefc721134afbfe3dc0179f5e928477
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2023-09-24 12:06:30 -0400

    dismiss notes permanently

--- static/form.js
+++ static/form.js
@@ -1,20 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     const alert = document.getElementById('disclaimer');
+    const dismissLink = document.getElementById('dismiss');
     const fileInput = document.getElementById('file-upload');
     const sendBtn = document.getElementById('sendBtn');
     const checkmarkSpan = document.getElementById('checkmark');
     const sendIcon = document.getElementById("sendIcon");
     const spinner = document.getElementById("spinner");
     
+    // Check if the alert has been closed.
+    if (sessionStorage.getItem('alertClosed') === 'true') {
+        alert.style.display = 'none';
+    }
+
     // Check if the alert has been dismissed.
-    if (sessionStorage.getItem('alertDismissed') === 'true') {
+    if (localStorage.getItem('alertDismissed') === 'true') {
         alert.style.display = 'none';
     }
     
+    // Handle click event on the "Dismiss" link.
+    dismissLink.addEventListener('click', function(e) {
+        e.preventDefault(); // Prevent default action (navigation).
+        localStorage.setItem('alertDismissed', 'true'); // Set item in localStorage.
+        new bootstrap.Alert(alert).close(); // Close the alert.
+    });
+
     // Listen for the close event of the alert.
     alert.addEventListener('close.bs.alert', function() {
-        // When the alert is dismissed, set the item in sessionStorage.
-        sessionStorage.setItem('alertDismissed', 'true');
+        // When the alert is closed, set the item in sessionStorage.
+        sessionStorage.setItem('alertClosed', 'true');
     });
     
     fileInput.addEventListener('change', function() {
