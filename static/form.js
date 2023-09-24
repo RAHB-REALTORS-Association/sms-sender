0fd9d5eab65bf5398fbb40d6e5801bdcf0f4d234
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2023-09-24 10:09:00 -0400

    dismiss persist

--- static/form.js
+++ static/form.js
@@ -1,20 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const alert = document.getElementById('disclaimer');
     const fileInput = document.getElementById('file-upload');
     const sendBtn = document.getElementById('sendBtn');
     const checkmarkSpan = document.getElementById('checkmark');
     const sendIcon = document.getElementById("sendIcon");
     const spinner = document.getElementById("spinner");
     
+    // Check if the alert has been dismissed.
+    if (sessionStorage.getItem('alertDismissed') === 'true') {
+        alert.style.display = 'none';
+    }
+    
+    // Listen for the close event of the alert.
+    alert.addEventListener('close.bs.alert', function() {
+        // When the alert is dismissed, set the item in sessionStorage.
+        sessionStorage.setItem('alertDismissed', 'true');
+    });
+    
     fileInput.addEventListener('change', function() {
         if (fileInput.files.length > 0) {
-            checkmarkSpan.classList.remove('d-none'); // Show checkmark if a file is uploaded.
+            // Show checkmark if a file is uploaded.
+            checkmarkSpan.classList.remove('d-none');
         } else {
-            checkmarkSpan.classList.add('d-none'); // Hide checkmark if no file is uploaded.
+            // Hide checkmark if no file is uploaded.
+            checkmarkSpan.classList.add('d-none');
         }
     });
 
     sendBtn.addEventListener("click", function() {
-        sendIcon.classList.add("d-none"); // Hide send icon
-        spinner.classList.remove("d-none"); // Show spinner
+        sendIcon.classList.add("d-none"); // Hide send icon.
+        spinner.classList.remove("d-none"); // Show spinner.
     });
 });
